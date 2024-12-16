fb3782f8b9dd6549839fdec4d8553a2699bc0254
BigBrownBag <maraykin@gmail.com>
2024-12-16 06:55:21 +0500

    add model

--- src/SoftEngine/Device.ts
+++ src/SoftEngine/Device.ts
@@ -1,7 +1,6 @@
 import { Color4, Vector3, Matrix, Vector2 } from "@babylonjs/core";
 import Mesh from "./Mesh";
 import Camera from "./Camera";
-
 export default class Device {
     private backBuffer: ImageData;
     private workingCanvas: HTMLCanvasElement;
@@ -50,45 +49,137 @@ export default class Device {
 
     // Project takes some 3D coordinates and transform them
     // in 2D coordinates using the transformation matrix
-    private project(coord: Vector3, transMat: Matrix): Vector2 {
+    private project(coord: Vector3, transMat: Matrix): Vector3 {
         // transforming the coordinates
         const point = Vector3.TransformCoordinates(coord, transMat);
         // The transformed coordinates will be based on coordinate system
         // starting on the center of the screen. But drawing on screen normally starts
         // from top left. We then need to transform them again to have x:0, y:0 on top left.
-        const x = point.x * this.workingWidth + this.workingWidth / 2.0 >> 0;
-        const y = -point.y * this.workingHeight + this.workingHeight / 2.0 >> 0;
-        return (new Vector2(x, y));
+        const x = point.x * this.workingWidth + this.workingWidth / 2.0;
+        const y = -point.y * this.workingHeight + this.workingHeight / 2.0;
+        return new Vector3(x, y, point.z);
     }
 
     // drawPoint calls putPixel but does the clipping operation before
-    private drawPoint(point: Vector2): void {
+    private drawPoint(point: Vector2, color: Color4): void {
         // Clipping what's visible on screen
         if (point.x >= 0 && point.y >= 0 && point.x < this.workingWidth && point.y < this.workingHeight) {
             // Drawing a red point
-            this.putPixel(point.x, point.y, new Color4(1, 0, 0, 1));
+            this.putPixel(point.x, point.y, color);
         }
     }
 
-    //  Bresenham’s line algorithm implementation
-    private drawLine(firstPoint: Vector2, secondPoint: Vector2): void {
-        let x0 = firstPoint.x >> 0;
-        let y0 = firstPoint.y >> 0;
-        const x1 = secondPoint.x >> 0;
-        const y1 = secondPoint.y >> 0;
-        const dx = Math.abs(x1 - x0);
-        const dy = Math.abs(y1 - y0);
-        const sx = (x0 < x1) ? 1 : -1;
-        const sy = (y0 < y1) ? 1 : -1;
-        let err = dx - dy;
+    // Clamping values to keep them between 0 and 1
+    private clamp(value: number, min: number = 0, max: number = 1): number {
+        return Math.max(min, Math.min(value, max));
+    }
 
-        while (true) {
-            this.drawPoint(new Vector2(x0, y0));
+    // Interpolating the value between 2 vertices 
+    // min is the starting point, max the ending point
+    // and gradient the % between the 2 points
+    private interpolate(min: number, max: number, gradient: number) {
+        return min + (max - min) * this.clamp(gradient);
+    }
 
-            if ((x0 == x1) && (y0 == y1)) break;
-            let e2 = 2 * err;
-            if (e2 > -dy) err -= dy, x0 += sx;
-            if (e2 < dx) err += dx, y0 += sy;
+    // drawing line between 2 points from left to right
+    // papb -> pcpd
+    // pa, pb, pc, pd must then be sorted before
+    private processScanLine(y: number, pa: Vector3, pb: Vector3, pc: Vector3, pd: Vector3, color: Color4): void {
+        // Thanks to current Y, we can compute the gradient to compute others values like
+        // the starting X (sx) and ending X (ex) to draw between
+        // if pa.Y == pb.Y or pc.Y == pd.Y, gradient is forced to 1
+        var gradient1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
+        var gradient2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+
+        var sx = this.interpolate(pa.x, pb.x, gradient1) >> 0;
+        var ex = this.interpolate(pc.x, pd.x, gradient2) >> 0;
+
+        // drawing a line from left (sx) to right (ex) 
+        for (var x = sx; x < ex; x++) {
+            this.drawPoint(new Vector2(x, y), color);
+        }
+    }
+
+    private drawTriangle(p1: Vector3, p2: Vector3, p3: Vector3, color: Color4): void {
+        // Sorting the points in order to always have this order on screen p1, p2 & p3
+        // with p1 always up (thus having the Y the lowest possible to be near the top screen)
+        // then p2 between p1 & p3
+        if (p1.y > p2.y) {
+            var temp = p2;
+            p2 = p1;
+            p1 = temp;
+        }
+
+        if (p2.y > p3.y) {
+            var temp = p2;
+            p2 = p3;
+            p3 = temp;
+        }
+
+        if (p1.y > p2.y) {
+            var temp = p2;
+            p2 = p1;
+            p1 = temp;
+        }
+
+        // inverse slopes
+        var dP1P2: number; var dP1P3: number;
+
+        // http://en.wikipedia.org/wiki/Slope
+        // Computing slopes
+        if (p2.y - p1.y > 0)
+            dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
+        else
+            dP1P2 = 0;
+
+        if (p3.y - p1.y > 0)
+            dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
+        else
+            dP1P3 = 0;
+
+        // First case where triangles are like that:
+        // P1
+        // -
+        // -- 
+        // - -
+        // -  -
+        // -   - P2
+        // -  -
+        // - -
+        // -
+        // P3
+        if (dP1P2 > dP1P3) {
+            for (var y = p1.y >> 0; y <= p3.y >> 0; y++)
+            {
+                if (y < p2.y) {
+                    this.processScanLine(y, p1, p3, p1, p2, color);
+                }
+                else {
+                    this.processScanLine(y, p1, p3, p2, p3, color);
+                }
+            }
+        }
+        // First case where triangles are like that:
+        //       P1
+        //        -
+        //       -- 
+        //      - -
+        //     -  -
+        // P2 -   - 
+        //     -  -
+        //      - -
+        //        -
+        //       P3
+        else {
+            for (var y = p1.y >> 0; y <= p3.y >> 0; y++)
+            {
+                if (y < p2.y) {
+                    this.processScanLine(y, p1, p2, p1, p3, color);
+                }
+                else {
+                    this.processScanLine(y, p2, p3, p1, p3, color);
+                }
+            }
         }
     }
 
@@ -120,10 +211,76 @@ export default class Device {
                 const pixelB = this.project(vertexB, transformMatrix);
                 const pixelC = this.project(vertexC, transformMatrix);
 
-                this.drawLine(pixelA, pixelB);
-                this.drawLine(pixelB, pixelC);
-                this.drawLine(pixelC, pixelA);
+                const color: number = 0.25 + ((+i % currentMesh.faces.length) / currentMesh.faces.length) * 0.75;
+                this.drawTriangle(pixelA, pixelB, pixelC, new Color4(color, color, color, 1));
             }
         }
+    }
+
+    private createMeshesFromData(meshData: any): Mesh[] {
+            const meshes: Mesh[] = [];
+            for (let meshIndex = 0; meshIndex < meshData.meshes.length; meshIndex++) {
+                const verticesArray: number[] = meshData.meshes[meshIndex].vertices;
+                // Faces
+                const indicesArray: number[] = meshData.meshes[meshIndex].indices;
+
+                const uvCount: number = meshData.meshes[meshIndex].uvCount;
+                let verticesStep = 1;
+
+                // Depending of the number of texture's coordinates per vertex
+                // we're jumping in the vertices array  by 6, 8 & 10 windows frame
+                switch (uvCount) {
+                    case 0:
+                        verticesStep = 6;
+                        break;
+                    case 1:
+                        verticesStep = 8;
+                        break;
+                    case 2:
+                        verticesStep = 10;
+                        break;
+                }
+
+                // the number of interesting vertices information for us
+                const verticesCount = verticesArray.length / verticesStep;
+                // number of faces is logically the size of the array divided by 3 (A, B, C)
+                const facesCount = indicesArray.length / 3;
+                const mesh = new Mesh(meshData.meshes[meshIndex].name, verticesCount, facesCount);
+                
+                // Filling the Vertices array of our mesh first
+                for (let index = 0; index < verticesCount; index++) {
+                    const x = verticesArray[index * verticesStep];
+                    const y = verticesArray[index * verticesStep + 1];
+                    const z = verticesArray[index * verticesStep + 2];
+                    mesh.vertices[index] = new Vector3(x, y, z);
+                }
+                
+                // Then filling the Faces array
+                for (let index = 0; index < facesCount; index++) {
+                    const a = indicesArray[index * 3];
+                    const b = indicesArray[index * 3 + 1];
+                    const c = indicesArray[index * 3 + 2];
+                    mesh.faces[index] = { a, b, c };
+                }
+                
+                // Getting the position you've set in Blender
+                const position = meshData.meshes[meshIndex].position;
+                mesh.position = new Vector3(position[0], position[1], position[2]);
+                meshes.push(mesh);
+            }
+            return meshes;
+        }
+
+    public uploadFile(fileName: string, callback: (result: Mesh[]) => void): void {
+        const connect = new XMLHttpRequest();
+        connect.overrideMimeType("application/babylon");
+        connect.onreadystatechange = () => {
+            if (connect.readyState == 4 && connect.status == 200) {
+                const meshData = JSON.parse(connect.responseText);
+                callback(this.createMeshesFromData(meshData));
+            }
+        };
+        connect.open("GET", fileName);
+        connect.send(null);
     }
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,8 @@
 import path from 'path';
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import TerserWebpackPlugin from "terser-webpack-plugin";
+// import TerserWebpackPlugin from "terser-webpack-plugin";
 import ESLintWebpackPlugin from "eslint-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 
 const isDev = process.env.NODE_ENV === "development";
 
@@ -15,26 +16,34 @@ module.exports = {
         path: path.resolve(__dirname, "build"),
         clean: true
     },
-    optimization: {
-        splitChunks: {
-            chunks: "all",
-        },
-        minimizer: [new TerserWebpackPlugin()],
-    },
+    // optimization: {
+    //     splitChunks: {
+    //         chunks: "all",
+    //     },
+    //     minimizer: [new TerserWebpackPlugin()],
+    // },
     devServer: isDev ? {
         port: 4200,
         hot: isDev,
     } : undefined,
-    devtool: isDev ? "source-map" : false,
+    devtool: "source-map",
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "public", "index.html"),
             favicon: path.resolve(__dirname, "public",  "favicon.ico")
         }),
-        new ESLintWebpackPlugin()
+        new ESLintWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "monkey.babylon"),
+                    to: path.resolve(__dirname, "build"),
+                },
+            ],
+        }),
     ],
     resolve: {
-      extensions: ['.ts', '.js', '.html'],
+      extensions: ['.ts', '.js'],
     },
     module: {
         rules: [
